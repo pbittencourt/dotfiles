ff10079199fa39d2d9ca996fbdd450da919dfa21
pbittencourt <pedrogabrieltt@gmail.com>
2023-07-29 15:44:09 -0300

    themes
    adaptamos o theme_switcher para não impactar a versão de repositório dos
    arquivos de `i3/`. as alterações nos themas gruvbox são secundárias,
    resquícios da escrita antiga que mexia no arquivo do repositório,
    consertado em commit anterior.

--- themes/theme_switcher.sh
+++ themes/theme_switcher.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
 full_path=$(realpath $0)
-echo $full_path
+#echo $full_path
 dir_path=$(dirname $full_path)
-echo $dir_path
+#echo $dir_path
 
 themes_list=$(cd $dir_path; echo */ | sed -e 's/\///g')
-echo $themes_list
+#echo $themes_list
 theme=$(echo $themes_list | sed -e 's/\s/\n/g' | rofi -dmenu -l 4 -p "")
-echo $theme
+#echo $theme
 
 if [ ! -z "$theme" ]; then
     notify-send -u normal -t 1500 "Tema boladão" "Aplicando tema $theme ..."
@@ -26,6 +26,14 @@ if [ ! -z "$theme" ]; then
     magenta=$(cat "$dir_path/$theme/xresources" | grep "^\*color5:.*" | awk '{ print $2}')
     cyan=$(cat "$dir_path/$theme/xresources" | grep "^\*color6:.*" | awk '{ print $2}')
     white=$(cat "$dir_path/$theme/xresources" | grep "^\*color7:.*" | awk '{ print $2}')
+    dark_black=$(cat "$dir_path/$theme/xresources" | grep "^\*color8:.*" | awk '{ print $2}')
+    dark_red=$(cat "$dir_path/$theme/xresources" | grep "^\*color9:.*" | awk '{ print $2}')
+    dark_green=$(cat "$dir_path/$theme/xresources" | grep "^\*color10:.*" | awk '{ print $2}')
+    dark_yellow=$(cat "$dir_path/$theme/xresources" | grep "^\*color11:.*" | awk '{ print $2}')
+    dark_blue=$(cat "$dir_path/$theme/xresources" | grep "^\*color12:.*" | awk '{ print $2}')
+    dark_magenta=$(cat "$dir_path/$theme/xresources" | grep "^\*color13:.*" | awk '{ print $2}')
+    dark_cyan=$(cat "$dir_path/$theme/xresources" | grep "^\*color14:.*" | awk '{ print $2}')
+    dark_white=$(cat "$dir_path/$theme/xresources" | grep "^\*color15:.*" | awk '{ print $2}')
 
     # gtk
     cat "$dir_path/$theme/gtk" > ~/.gtkrc-2.0
@@ -36,7 +44,7 @@ if [ ! -z "$theme" ]; then
     sed -i -e 's/^include.*/'"${escaped_kitty}"'/g' ~/.config/kitty/kitty.conf
 
     # vim
-    cat ~/.vimrc-base "$dir_path/$theme/vim" > ~/.vimrc
+    cat ~/dotfiles/vim/base "$dir_path/$theme/vim" > ~/.vimrc
 
     # rofi
     rofi="theme: \"$dir_path/$theme/rofi\";"
@@ -44,61 +52,85 @@ if [ ! -z "$theme" ]; then
     sed -i -e 's/theme.*/'"${escaped_rofi}"'/g' ~/.config/rofi/config.rasi
 
     # conky
-    sed -i -e "s/default_color = '.*'/default_color = '${foreground}'/g" ~/.config/conky/conky.conf
-    sed -i -e "s/color1 = '.*'/color1 = '${green}'/g" ~/.config/conky/conky.conf
-    sed -i -e "s/color2 = '.*'/color2 = '${cyan}'/g" ~/.config/conky/conky.conf
+    target="/home/monolito/dotfiles/conky/conky.conf"
+    destination="/home/monolito/.config/conky/conky.conf"
+    sed -e "s/color2.*=.*'.*'/color2 = '${yellow}'/g" $target > $destination
+    sed -i -e "s/color1.*=.*'.*'/color1 = '${dark_yellow}'/g" $destination
+    sed -i -e "s/default_color.*=.*'.*'/default_color = '${foreground}'/g" $destination
+    target="/home/monolito/dotfiles/conky/music.conf"
+    destination="/home/monolito/.config/conky/music.conf"
+    sed -e "s/color2.*=.*'.*'/color2 = '${yellow}'/g" $target > $destination
+    sed -i -e "s/color1.*=.*'.*'/color1 = '${dark_yellow}'/g" $destination
+    sed -i -e "s/default_color.*=.*'.*'/default_color = '${foreground}'/g" $destination
+    target="/home/monolito/dotfiles/conky/system.conf"
+    destination="/home/monolito/.config/conky/system.conf"
+    sed -e "s/color2.*=.*'.*'/color2 = '${yellow}'/g" $target > $destination
+    sed -i -e "s/color1.*=.*'.*'/color1 = '${dark_yellow}'/g" $destination
+    sed -i -e "s/default_color.*=.*'.*'/default_color = '${foreground}'/g" $destination
+    sed -i -e "s/exploded\/\w*/exploded\/$theme/g" $destination
 
     # i3
     # i3blocks main
-    sed -i -e "s/.*title_color/color=${yellow} \#title_color/g" ~/.config/i3/i3blocks.conf
-    sed -i -e "s/.*alt_color/color=${blue} \#alt_color/g" ~/.config/i3/i3blocks.conf
-    sed -i -e "s/.*kb_color/color=${green} \#kb_color/g" ~/.config/i3/i3blocks.conf
+    target="/home/monolito/dotfiles/i3/i3blocks.conf"
+    destination="/home/monolito/.config/i3/i3blocks.conf"
+    #sed -i -e "s/.*title_color/color=${yellow} \#title_color/g" ~/.config/i3/i3blocks.conf
+    #sed -i -e "s/.*alt_color/color=${blue} \#alt_color/g" ~/.config/i3/i3blocks.conf
+    #sed -i -e "s/.*kb_color/color=${green} \#kb_color/g" ~/.config/i3/i3blocks.conf
+    sed -e "s/.*title_color/color=${yellow} \#title_color/g" $target > $destination
+    sed -i -e "s/.*alt_color/color=${blue} \#alt_color/g" $destination
+    sed -i -e "s/.*kb_color/color=${green} \#kb_color/g" $destination
     # i3blocks keyindicator
-    sed -i -e 's/my \$color_on.*/my \$color_on  = \"'"${magenta}"'\";/g' ~/.config/i3/i3blocks/keyindicator
-    sed -i -e 's/my \$color_off.*/my \$color_off = \"'"${background}"'\";/g' ~/.config/i3/i3blocks/keyindicator
+    target="/home/monolito/dotfiles/i3/i3blocks/keyindicator"
+    destination="/home/monolito/.config/i3/i3blocks/keyindicator"
+    #sed -i -e 's/my \$color_on.*/my \$color_on  = \"'"${magenta}"'\";/g' ~/.config/i3/i3blocks/keyindicator
+    #sed -i -e 's/my \$color_off.*/my \$color_off = \"'"${background}"'\";/g' ~/.config/i3/i3blocks/keyindicator
+    sed -e 's/my \$color_on.*/my \$color_on  = \"'"${magenta}"'\";/g' $target > $destination
+    sed -i -e 's/my \$color_off.*/my \$color_off = \"'"${background}"'\";/g' $destination
     # i3blocks cpu_usage
-    sed -i -e 's/.*crit_color.*/    print \"'"${yellow}"'\\n\"; #crit_color/g' ~/.config/i3/i3blocks/cpu_usage
-    sed -i -e 's/.*alt_color.*/    print \"'"${red}"'\\n\"; #alt_color/g' ~/.config/i3/i3blocks/cpu_usage
+    target="/home/monolito/dotfiles/i3/i3blocks/cpu_usage"
+    destination="/home/monolito/.config/i3/i3blocks/cpu_usage"
+    #sed -i -e 's/.*crit_color.*/    print \"'"${red}"'\\n\"; #crit_color/g' ~/.config/i3/i3blocks/cpu_usage
+    #sed -i -e 's/.*alt_color.*/    print \"'"${yellow}"'\\n\"; #alt_color/g' ~/.config/i3/i3blocks/cpu_usage
+    sed -e 's/.*crit_color.*/    print \"'"${red}"'\\n\"; #crit_color/g' $target > $destination
+    sed -i -e 's/.*alt_color.*/    print \"'"${yellow}"'\\n\"; #alt_color/g' $destination
     i3-msg restart
 
+    # wallpaper
+    xsetroot -solid "$background"
     case $theme in
         everlight)
-            # wallpaper
-            nitrogen --set-zoom-fill ~/Imagens/Wallpapers/ben-neale-29w9FiMWSr8-unsplash_flip.jpg
+            #nitrogen --set-zoom-fill ~/Imagens/Wallpapers/ben-neale-29w9FiMWSr8-unsplash_flip.jpg
             ;;
         everdark)
-            # wallpaper
-            nitrogen --set-zoom-fill ~/Imagens/Wallpapers/roads.png
+            #nitrogen --set-zoom-fill ~/Imagens/Wallpapers/roads.png
             ;;
         gruvlight)
-            # wallpaper
-            nitrogen --set-zoom-fill ~/Imagens/Wallpapers/wallhaven-mdqkyk.png
+            #nitrogen --set-zoom-fill ~/Imagens/Wallpapers/colorwalls.jpg
             ;;
         gruvdark)
-            # wallpaper
-            nitrogen --set-color=#070705 --set-centered ~/Imagens/Wallpapers/ascent1500.jpg
+            #nitrogen --set-color=#070705 --set-centered ~/Imagens/Wallpapers/ascent1500.jpg
             ;;
     esac
 
     # dunst
-    dunst="
-    [urgency_low]\n
-        background = \"${background}\"\n
-        foreground = \"${green}\"\n
-        frame_color = \"${green}\"\n
-        timeout = 5\n
-    [urgency_normal]\n
-        background = \"${background}\"\n
-        foreground = \"${yellow}\"\n
-        frame_color = \"${yellow}\"\n
-        timeout = 5\n
-    [urgency_critical]\n
-        background = \"${red}\"\n
-        foreground = \"${white}\"\n
-        frame_color = \"${white}\"\n
-        timeout = 0\n
-    "
-    echo $dunst > "$dir_path/$theme/dunst"
+    #dunst="
+    #[urgency_low]\n
+    #    background = \"${background}\"\n
+    #    foreground = \"${green}\"\n
+    #    frame_color = \"${green}\"\n
+    #    timeout = 5\n
+    #[urgency_normal]\n
+    #    background = \"${background}\"\n
+    #    foreground = \"${yellow}\"\n
+    #    frame_color = \"${yellow}\"\n
+    #    timeout = 5\n
+    #[urgency_critical]\n
+    #    background = \"${red}\"\n
+    #    foreground = \"${white}\"\n
+    #    frame_color = \"${white}\"\n
+    #    timeout = 0\n
+    #"
+    #echo $dunst > "$dir_path/$theme/dunst"
     cat ~/dotfiles/dunst/dunstrc-base "$dir_path/$theme/dunst" > ~/.config/dunst/dunstrc
     pkill dunst
     dunst -config ~/.config/dunst/dunstrc &
