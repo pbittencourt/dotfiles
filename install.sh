16ea2c9171277af598badcc78cc3c82c16faf05d
pbittencourt <pedrogabrieltt@gmail.com>
2023-07-30 12:44:40 -0300

    install check dotfiles init
    Começamos validação para clonar/atualizar repositório com dotfiles.
    Também faremos uma primeira rodada para criação de links simbólicos.

--- install.sh
+++ install.sh
@@ -3,22 +3,50 @@
 # let's create a brand new fresh enviroment!
 # ~ <3
 
-echo "WELCOME BACK, MR MONOLITO.\n"
+echo "---------------------------"
+echo " WELCOME BACK, MR MONOLITO "
+echo "---------------------------\n"
+
+echo "Daremos início à instalação do ambiente."
+read -p "Deseja continuar? (y/n) __ " yn
+case $yn in
+    [^yYsS] )
+        echo "\nGoodbye!"
+        exit 1
+esac
 
 # initial update
-sudo xbps-install -Su
+#sudo xbps-install -Su
 
 # nonfree repo
-sudo xbps-install -Sy void-repo-nonfree
+#sudo xbps-install -Sy void-repo-nonfree
 
 # apps
-sudo xbps-install -Sy i3-gaps i3lock i3status i3blocks \
-    git vim gvim kitty qutebrowser rsync \
-    scrot feh flameshot nitrogen lxappearance inkscape \
-    nm-applet redshift dunst rofi greenclip xcalc \
-    mpd mpc mpv vlc ncmpcpp minidlna
+#sudo xbps-install -y i3-gaps i3lock i3status i3blocks \
+#    git vim gvim kitty qutebrowser rsync \
+#    scrot feh flameshot nitrogen lxappearance inkscape \
+#    nm-applet redshift dunst rofi greenclip xcalc \
+#    mpd mpc mpv vlc ncmpcpp minidlna ranger
 
-# final update
-sudo xbps-install -Su
+# clone dotfiles
+dotfiles="$HOME/dotfiles"
+if [ -d $dotfiles ]; then
+    echo "O diretório $dotfiles já existe."
+    cd $dotfiles
+    if [ -d .git ]; then
+        echo "Esse diretório é um repositório git. Vamos atualizá-lo."
+        git pull
+    else
+        echo "Esse diretório não é um repositório git. Verifique!"
+        exit 1
+    fi
+else
+    echo "O diretório $dotfiles não existe. Vamos clonar o repositório."
+    git clone https://github.com/pbittencourt/dotfiles.git
+fi
+
+# symlinks
+# ... TODO finalizar
+sudo ln -s "$dotfiles/rofi/" "$HOME/.config"
 
 echo "\nENJOY!"
