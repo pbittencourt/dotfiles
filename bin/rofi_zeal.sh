7a270b3f488b763cc422c352d73b624ff15e16e6
monolito_void <pedrogabrieltt@gmail.com>
2024-02-18 20:53:14 -0300

    rofi_zeal with browser
    changed rofi_zeal to match default browser (now qutebrowser)

--- bin/rofi_zeal.sh
+++ bin/rofi_zeal.sh
@@ -2,6 +2,7 @@
 # A quick documentation file-finder based on rofi and zeal
 # Requires: rofi, zeal, xfce4-terminal nerdfonts
 files=/var/tmp/rofi-zeal_term_list
+browser=qutebrowser
 
 append_new_term() {
   # Delete term. Append on the first line.
@@ -37,40 +38,40 @@ d)
   # Search on DuckDuckGo
   append_new_term
   mantoread=$(echo "$input" | cut -c 2- | xargs)
-  exec brave-browser "https://duckduckgo.com/?q=$mantoread" &> /dev/null &
+  exec $browser "https://duckduckgo.com/?q=$mantoread" &> /dev/null &
   ;;
 g)
   # Search on Google
   append_new_term
   mantoread=$(echo "$input" | cut -c 2- | xargs)
-  exec brave-browser "https://www.google.com/search?q=$mantoread" &> /dev/null &
+  exec $browser "https://www.google.com/search?q=$mantoread" &> /dev/null &
   ;;
 b)
   # Search on Brave
   append_new_term
   mantoread=$(echo "$input" | cut -c 2- | xargs)
-  exec brave-browser "https://search.brave.com/search?q=$mantoread" &> /dev/null &
+  exec $browser "https://search.brave.com/search?q=$mantoread" &> /dev/null &
   ;;
 s)
-  # Search on stack overflow
+  # Search on StackOverflow
   append_new_term
   mantoread=$(echo "$input" | cut -c 2- | xargs)
-  exec brave-browser "https://stackoverflow.com/search?q=$mantoread" &> /dev/null &
+  exec $browser "https://stackoverflow.com/search?q=$mantoread" &> /dev/null &
   ;;
 i)
-  # Search on imdb
+  # Search on IMDB
   append_new_term
   mantoread=$(echo "$input" | cut -c 2- | xargs)
-  exec brave-browser "https://www.imdb.com/find/?q=$mantoread" &> /dev/null &
+  exec $browser "https://www.imdb.com/find/?q=$mantoread" &> /dev/null &
   ;;
 o)
-  # Search on opensubtitles
+  # Search on OpenSubtitles
   append_new_term
   mantoread=$(echo "$input" | cut -c 2- | xargs)
-  exec brave-browser "https://www.opensubtitles.org/pt/search/sublanguageid-por,pob/moviename-$mantoread" &> /dev/null &
+  exec $browser "https://www.opensubtitles.org/pt/search/sublanguageid-por,pob/moviename-$mantoread" &> /dev/null &
   ;;
 t)
-  # Search on tldr
+  # Search on TLDR
   append_new_term
   mantoread=$(echo "$input" | cut -c 2- | xargs)
   exec kitty --name floating -e "tldr $mantoread" &> /dev/null &
@@ -79,8 +80,11 @@ med)
   # Search on Medium
   append_new_term
   mantoread=$(echo "$input" | cut -c 4- | xargs)
-  exec brave-browser "https://medium.com/search?q=$mantoread" &> /dev/null &
+  exec $browser "https://medium.com/search?q=$mantoread" &> /dev/null &
   ;;
+?)
+  # print help
+  exec xfce4-terminal --title=floating -e 'cat ~/dotfiles/bin/rofizealhelp.md'
 *)
   # Open zeal only if there's text input
   if [ ! -z "$input" ]
