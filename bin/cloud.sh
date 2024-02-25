496503f3f4770f2c501aac18ce56894de2c968f8
monolito_void <pedrogabrieltt@gmail.com>
2024-02-25 10:12:10 -0300

    minor mods on cloud and songinfo scripts
    rclone is now temporary disabled -- we have to evaluate its utilization
    
    lyrics fetching ends on a infinite loop when the song simply does not
    have lyrics -- think about instrumental songs. we have to implement a new
    script, but let keep this to the future self

--- bin/cloud.sh
+++ bin/cloud.sh
@@ -12,12 +12,12 @@ else
         "Dropbox não iniciado!\nVerifique o log para maiores informações."
 fi
 
-if rclone mount --daemon --vfs-cache-mode full gdrive:/ ./google_drive/ ; then
-    notify-send \
-        -t 5000 -i "emblem-synchronizing-symbolic" \
-        "Rclone" "Rclone iniciado com sucesso!"
-else
-    notify-send \
-        -u critical -t 5000 -i "emblem-unsynchronized-symbolic" "Rclone" \
-        "Rclone não iniciado!\nVerifique o log para maiores informações."
-fi
+#if rclone mount --daemon --vfs-cache-mode full gdrive:/ ./google_drive/ ; then
+#    notify-send \
+#        -t 5000 -i "emblem-synchronizing-symbolic" \
+#        "Rclone" "Rclone iniciado com sucesso!"
+#else
+#    notify-send \
+#        -u critical -t 5000 -i "emblem-unsynchronized-symbolic" "Rclone" \
+#        "Rclone não iniciado!\nVerifique o log para maiores informações."
+#fi
