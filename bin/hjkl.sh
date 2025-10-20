3748f759c6d8583d80796b92229446523ae86abe
monolito_void <pedrogabrieltt@gmail.com>
2025-10-20 08:30:50 -0300

    feat(bin): new scripts
    added some scripts to track.

--- bin/hjkl.sh
+++ bin/hjkl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-setxkbmap -layout us -variant intl
-#xmodmap ~/dotfiles/xmodmap;
+setxkbmap -layout us -variant intl;
+#xmodmap ~/dotfiles/xmodmap_original;
 #xcape -e 'Mode_switch=Escape';
 notify-send -u low -t 1800 -i "keyboard" "HJKL" "Conectado com sucesso!";
